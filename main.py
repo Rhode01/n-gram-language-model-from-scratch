1dae24eb9ff3e7d05a61c34a1dfd0ee6caa4bba5
Rhode01 <Rhodrickmhone@gmail.com>
2024-07-18 15:14:35 +0200

    finished

--- main.py
+++ main.py
@@ -11,7 +11,33 @@ sample2 = [
  ('dangerous', 'man', '.')
 ]
 
-def building_n_gram(token:List[str], n:int) -> List[Tuple[str]]:
+def build_n_grams(token:List[str], n:int) -> List[Tuple[str]]:
     pass
 
-building_n_gram(sample1,3)
+build_n_grams(sample1,3)
+
+BOS = '<BOS>'
+EOS = '<EOS>'
+def build_ngrams_ctrl(tokens: List[str], n: int) -> List[Tuple[str]]:
+    pass
+   
+
+build_ngrams_ctrl(sample1, n=3)
+assert len (build_ngrams_ctrl(sample1, n=3)) 
+from typing import Dict
+def build_ngram_model(texts: List[List[str]], n: int) -> Dict[Tuple[str, ...], Dict[str, float]]:
+    pass
+    
+build_ngram_model([sample1, sample2], n=3)
+
+full_text = []
+with open('arthur-conan-doyle.tok.train.txt', 'rt') as fin:
+    for line in fin:
+        full_text.append(list(line.split()))
+model = build_ngram_model(full_text, n=3)
+for prefix in [(BOS, BOS), (BOS, 'It'), ('It', 'was'), ('my', 'dear')]:
+    print(*prefix)
+    sorted_probs = sorted(model[prefix].items(), key=lambda x: -x[1])
+    for k, v in sorted_probs[:5]:
+        print(f'\t{k}\t{v:.4f}')
+    print(f'\t[{len(sorted_probs)-5} more...]')
